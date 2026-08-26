03f22fb478d4dccbe9bb361ddb031f573bb28157
oa4170987-ship-it <oa4170987@gmail.com>
2026-08-26 09:32:20 +0300

    Update index.py

--- index.py
+++ index.py
@@ -21,7 +21,7 @@ def chat():
             "Content-Type": "application/json"
         }
         payload = {
-            "model": "llama3-8b-8192",
+            "model": "llama-3.3-70b-versatile",
             "messages": [
                 {"role": "user", "content": user_message}
             ]
